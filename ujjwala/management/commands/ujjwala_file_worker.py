cdf6f03482dc59ac3e6fbe6498014ce1e1f9fb21
sahilocean <sahil90.ocean@gmail.com>
2022-07-23 10:35:21 +0530

    [Update] compression code

--- ujjwala/management/commands/ujjwala_file_worker.py
+++ ujjwala/management/commands/ujjwala_file_worker.py
@@ -13,7 +13,7 @@ from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from tusclient import client
 
-from ujjwala.models import UjjwalaV2Application, ConnectionDisbursement
+from ujjwala.models import UjjwalaV2Application, ConnectionDisbursement, PreInspection
 
 
 def get_tus_client():
@@ -79,26 +79,24 @@ class Command(BaseCommand):
             for customer_doc in dc.documents.all():
                 print("PreInspection Doc {} {}".format(customer_doc.type, customer_doc.link))
                 success, upload_url, file_size = upload_compressed_file_to_tus(customer_doc.link)
-                if success:
-                    customer_doc.file_size = file_size
-                    if not customer_doc.link == upload_url:
-                        customer_doc.link = upload_url
-                        customer_doc.front_compressed = True
+                customer_doc.file_size = file_size
+                if success and not customer_doc.link == upload_url:
+                    customer_doc.link = upload_url
+                    customer_doc.compressed = True
                 customer_doc.save()
 
     def handle_disb(self, *args, **options):
         for dc in ConnectionDisbursement.objects.filter(
-                updated_on__date__lt=datetime.datetime.today().date()
+            updated_on__date__lt=datetime.datetime.today().date()
         ).order_by('-id'):
             print("\n\n\nProcessing Files For disbersment : {}".format(dc.id))
             for customer_doc in dc.documents.all():
                 print("Disb Doc {} {}".format(customer_doc.type, customer_doc.link))
                 success, upload_url, file_size = upload_compressed_file_to_tus(customer_doc.link)
-                if success:
-                    customer_doc.file_size = file_size
-                    if not customer_doc.link == upload_url:
-                        customer_doc.link = upload_url
-                        customer_doc.front_compressed = True
+                customer_doc.file_size = file_size
+                if success and not customer_doc.link == upload_url:
+                    customer_doc.link = upload_url
+                    customer_doc.compressed = True
                 customer_doc.save()
 
     def handle_application(self, *args, **options):
@@ -108,10 +106,9 @@ class Command(BaseCommand):
             for customer_doc in application.documents.all():
                 print("Customer Doc {} {}".format(customer_doc.type, customer_doc.link))
                 success, upload_url, file_size = upload_compressed_file_to_tus(customer_doc.link)
-                if success:
-                    if not customer_doc.link == upload_url:
-                        customer_doc.link = upload_url
-                        customer_doc.compressed = True
+                if success and not customer_doc.link == upload_url:
+                    customer_doc.link = upload_url
+                    customer_doc.compressed = True
 
                 customer_doc.file_size = file_size
                 customer_doc.save()
@@ -119,20 +116,18 @@ class Command(BaseCommand):
             for family_member in application.family_members.all():
                 print("UID Front {}".format(family_member.uid_front_link))
                 success, upload_url, file_size = upload_compressed_file_to_tus(family_member.uid_front_link)
-                if success:
-                    if not family_member.uid_front_link == upload_url:
-                        family_member.uid_front_link = upload_url
-                        family_member.uid_front_compressed = True
+                if success and not family_member.uid_front_link == upload_url:
+                    family_member.uid_front_link = upload_url
+                    family_member.uid_front_compressed = True
 
                 family_member.uid_front_file_size = file_size
                 family_member.save()
 
                 print("UID Back {}".format(family_member.uid_back_link))
                 success, upload_url, file_size = upload_compressed_file_to_tus(family_member.uid_back_link)
-                if success:
-                    if not family_member.uid_back_link == upload_url:
-                        family_member.uid_back_link = upload_url
-                        family_member.uid_back_compressed = True
+                if success and not family_member.uid_back_link == upload_url:
+                    family_member.uid_back_link = upload_url
+                    family_member.uid_back_compressed = True
 
                 family_member.uid_back_file_size = file_size
                 family_member.save()
