46514de1b0f0e34925b0b751bd01ea0367300034
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2022-09-29 11:26:57 +0530

    Resize Image Function

--- app_utilities/templatetags/img_utils.py
+++ app_utilities/templatetags/img_utils.py
@@ -5,6 +5,7 @@ register = template.Library()
 
 @register.filter()
 def resize(url, fit_in_size):
+	"""return f"http://dca.arungas.com:6988/unsafe/fit-in/{fit_in_size}/filters:format(webp)/{url}"""
 	if not url:
 		return ''
-	return f"http://dca.arungas.com:6988/unsafe/fit-in/{fit_in_size}/filters:format(webp)/{url}"
+	return f"http://dca.arungas.com:6988/unsafe/fit-in/{fit_in_size}/{url}"
