f09518b8f24157019e3ae9c507dfbfd7992d6699
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2022-10-08 17:23:37 +0530

    Permission Second Delivery

--- ujjwala/templatetags/ujjwala_utils.py
+++ ujjwala/templatetags/ujjwala_utils.py
@@ -1,6 +1,6 @@
 from django import template
 
-from ujjwala.ujjwala_functions import is_member_of_disbursement_drive
+from ujjwala.ujjwala_functions import is_member_of_disbursement_drive, is_member_of_second_cylinder_delivery
 
 register = template.Library()
 
@@ -8,3 +8,8 @@ register = template.Library()
 @register.filter()
 def has_disbursement_permission(user):
 	return is_member_of_disbursement_drive(user)
+
+
+@register.filter()
+def has_second_delivery_permission(user):
+	return is_member_of_second_cylinder_delivery(user)
