22b49782f84968d47abc45e536a0995948cb7e6a
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2023-09-26 10:40:29 +0530

    SDMS Services

--- sdms/services.py
+++ sdms/services.py
@@ -61,12 +61,79 @@ class IoclOmcDedup():
 			return "Technical Error, Kindly try again"
 
 
+	def __process_omc_dedup_result__v2(self, res_code):
+		"""
+		{
+		    "TRANSACID": "20230923121515-56592777",
+		    "CON_STATE": null,
+		    "CDP_ID": null,
+		    "KYC_ID": null,
+		    "CONSUMER_NO": null,
+		    "LPG_ID": null,
+		    "OMC_CODE": "3",
+		    "DEDUP_RESULT": "Reject",
+		    "DEMO_RESULT": null,
+		    "COUNTERPART": [
+		        {
+		            "CON_STATE": "Consumer",
+		            "CDP_ID": "1328115271",
+		            "KYC_ID": null,
+		            "LPG_ID": "10000000111494923",
+		            "OMC_CODE": "1",
+		            "DEDUP_TYP": "A",
+		            "DEDUP_GRPID": "1",
+		            "SUSPFLAG": null,
+		            "DISTR_CODE": "188177",
+		            "DISTR_NAME": "MANASWINI BHARATGAS GRAMIN VITRAK",
+		            "DISTR_ADDR1": "AT/PO ERABANGA",
+		            "DISTR_ADDR2": "PS GOP VIA BIRATUNG",
+		            "DISTR_ADDR3": null,
+		            "CONS_NAME": "KUNTALA BARAL",
+		            "CONS_NO": "111494923",
+		            "CONS_ADDR1": "- W/O-PABITRA MOHAN BARAL",
+		            "CONS_ADDR2": "AT-KHANDASAHI PO-RAHANGA",
+		            "CONS_ADDR3": "PURI Odisha",
+		            "LANDMARK": "PS-GOP",
+		            "VILL_TOWN": "409424",
+		            "PIN_CODE": "752110",
+		            "KYC_DATE": "20210821000000",
+		            "SV_DATE": null,
+		            "SEED_DATE": null,
+		            "INSTL_DATE": "20210907183500",
+		            "PPAC_DISTCODE": "2222",
+		            "PPAC_STATCODE": null,
+		            "CONS_STATUS": "1A",
+		            "RATION_CARD": null,
+		            "RELATIONCODE": "F",
+		            "FAM_MEMBRNAME": "PABITRA MOHAN BARAL",
+		            "ERROR_CODE": "ZZM_MSGCLS-015",
+		            "ERROR_MSG": "Duplicate Aadhaar found in CDP"
+		        }
+		    ]
+		}
+		"""
+		if resp.get('DEDUP_RESULT', ''):
+			return resp.get('COUNTERPART')
+		elif resp.get('ERRORS', ''):
+			errors = resp.get('ERRORS')
+			if errors[0]['ERROR_CODE'] == 0:
+				return "Success"
+		else:
+			return "Technical Error, Kindly try again"
+
+
 	def omc_aadhar_dedup(self, aadhar_no):
+		# resp = self.session.post(OMC_DEDUP_URL, data={
+		# 	'requestType': 'A',
+		# 	'aadharNo': aadhar_no,
+		# 	'account': '',
+		# 	'bankCode': '',
+		# })
 		resp = self.session.post(OMC_DEDUP_URL, data={
-			'requestType': 'A',
-			'aadharNo': aadhar_no,
-			'account': '',
-			'bankCode': '',
+			'requestType': '02',
+			'aadhaar': aadhar_no,
+			'atr': '',
+			'ifscState': '',
 		})
 		resp = resp.text
 
@@ -75,11 +142,12 @@ class IoclOmcDedup():
 			return self.omc_aadhar_dedup(aadhar_no)
 
 		resp = json.loads(resp)
-		return {
-			'HPCL': self.__process_omc_dedup_result__(resp[0]),
-			'BPLC': self.__process_omc_dedup_result__(resp[1]),
-			'IOCL': self.__process_omc_dedup_result__(resp[2])
-		}
+		# return {
+		# 	'HPCL': self.__process_omc_dedup_result__(resp[0]),
+		# 	'BPLC': self.__process_omc_dedup_result__(resp[1]),
+		# 	'IOCL': self.__process_omc_dedup_result__(resp[2])
+		# }
+		return resp
 
 
 if __name__ == '__main__':
