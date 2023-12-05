bfc5eb1e0b21e91524850664aea506d0072b5b8e
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2023-12-05 10:20:33 +0530

    IFSCode List Model

--- reference_data/admin.py
+++ reference_data/admin.py
@@ -4,10 +4,16 @@ from django.contrib import admin
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import TokensExcluded
+from .models import TokensExcluded, IFSCodeList
 
 
 @admin.register(TokensExcluded)
 class TokensExcludedAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'gender')
     search_fields = ('name', 'gender')
+
+
+@admin.register(IFSCodeList)
+class IFSCodeListAdmin(admin.ModelAdmin):
+    list_display = ('id', 'old_ifscode', 'new_ifscode')
+    search_fields = ('old_ifscode', 'new_ifscode')
--- reference_data/models.py
+++ reference_data/models.py
@@ -6,3 +6,9 @@ from reference_data.enums import TokenExcludeEnum
 class TokensExcluded(models.Model):
 	name = models.CharField(max_length=256)
 	gender = models.CharField(max_length=32, choices=TokenExcludeEnum.choices, default=TokenExcludeEnum.MALE)
+
+
+class IFSCodeList(models.Model):
+	old_ifscode = models.CharField(max_length=32)
+	new_ifscode = models.CharField(max_length=32)
+	main_branch = models.BooleanField(default=False)
