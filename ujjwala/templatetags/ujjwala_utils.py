48bcc47aab2dbfc42ec8f97bd7b73b30f73e2585
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2023-12-27 13:02:23 +0530

    TemplateTags Update

--- ujjwala/templatetags/ujjwala_utils.py
+++ ujjwala/templatetags/ujjwala_utils.py
@@ -1,7 +1,7 @@
 from django import template
 
 from ujjwala.ujjwala_functions import is_member_of_disbursement_drive, is_member_of_second_cylinder_delivery, \
-	is_member_of_reviewer_group
+	is_member_of_reviewer_group, can_resolve_service_request
 
 register = template.Library()
 
@@ -19,3 +19,13 @@ def has_second_delivery_permission(user):
 @register.filter()
 def has_review_permission(user):
 	return is_member_of_reviewer_group(user)
+
+
+@register.filter()
+def filter_status(queryset, status):
+	return queryset.filter(status=status)
+
+
+@register.filter()
+def has_service_request_resolve_permission(user):
+	return can_resolve_service_request(user)
