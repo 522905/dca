88a4b98fdaf07649b16b4e1d8f96e43e2d386247
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2024-02-14 15:57:06 +0530

    Comments XTD Urls

--- domestic_app/urls.py
+++ domestic_app/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     # path(r'', RedirectView.as_view(url='https://www.arungas.com/info', permanent=False)),
     # path(r'ujjwala/', include('ujjwala.urls')),
     # path(r'', RedirectView.as_view(url='/ujjwala/portal/application-status-search/', permanent=False)),
+    path(r'comments/', include('django_comments_xtd.urls')),
     path(r'', RedirectView.as_view(url='/ujjwala/portal/user_dashboard/', permanent=False)),
     path(r'', include(frontend_urls)),
 ]
