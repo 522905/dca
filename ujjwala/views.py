df65d21caa163f997f53f5d84cbf986723fac3aa
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2024-02-23 15:05:36 +0530

    Addres Update In Old Address

--- ujjwala/views.py
+++ ujjwala/views.py
@@ -4051,19 +4051,14 @@ class CamundaChangeAddressView(FormView):
 		return kwargs
 
 	def form_valid(self, form):
-		# obj = self.get_object()
 		data = form.clean()
-		# user = None
-		# if self.kwargs.get('message_source') == 'STAFF':
-		# 	user = get_current_user()
 		url = f"https://camunda.dca.arungas.com/engine-rest/message"
 		res = requests.post(url, json={
 			"messageName": "Message_review_addressnew_address_received",
 			'processInstanceId': self.kwargs.get('process_instance_id'),
 			"processVariables": {
+				"old_address_json": {"value": json.dumps(self.variables.get('address_json').get('value')), "type": "String"},
 				"address_json": {"value": json.dumps(data['address_json']), "type": "String"},
-				# "user_id": {"value": user.id if user else '', "type": "String"},
-				# "user_name": {"value": f"{user.first_name} {user.last_name}" if user else '', "type": "String"},
 				"message_source": {"value": f"{self.kwargs.get('message_source')}", "type": "String"},
 				"agent": {"value": f"{self.kwargs.get('agent', '').replace('+', '')}", "type": "String"},
 				"camunda_address_updated": {"value": "true", "type": "Boolean"}
