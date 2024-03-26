74ecb9a2eb5ba0cfbc06a1d1300c818d2f1e9073
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2024-03-26 18:46:03 +0530

    Connection App Sales Order Camunda Task Command

---
+++ connection_app/management/commands/connection_app_sales_order_scheduler.py
@@ -0,0 +1,32 @@
+import datetime
+
+from django.core.management import BaseCommand
+
+from ujjwala.camunda_functions import start_process_in_camunda_v2
+
+
+class Command(BaseCommand):
+	def add_arguments(self, parser):
+		# Positional arguments
+		parser.add_argument('from_date', type=str)
+		parser.add_argument('sales_order_type', type=str)
+
+	def handle(self, *args, **options):
+		from_date = options.get('from_date')
+		sales_order_type = options.get('sales_order_type', 'sales_order_invoice')
+
+		if not from_date:
+			from_date = datetime.datetime.today() - datetime.timedelta(days=1)
+			from_date = from_date.strftime('%d-%b-%Y')
+
+		variables = {
+			"variables":
+				{
+					"from_date": {"value": from_date, "type": "String"},
+					"sales_order_type": {"value": sales_order_type, "type": "String"},
+				}
+		}
+
+		print(options)
+		res = start_process_in_camunda_v2('Process_domestic_app', variables=variables)
+		print(res)
