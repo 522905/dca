46cd01c6c8e57b1075cc722c1041a74e0e46abca
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2024-03-26 18:54:48 +0530

    Connection App Scheduler Command Update

--- connection_app/management/commands/connection_app_sales_order_scheduler.py
+++ connection_app/management/commands/connection_app_sales_order_scheduler.py
@@ -8,16 +8,13 @@ from ujjwala.camunda_functions import start_process_in_camunda_v2
 class Command(BaseCommand):
 	def add_arguments(self, parser):
 		# Positional arguments
-		parser.add_argument('from_date', type=str)
-		parser.add_argument('sales_order_type', type=str)
+		parser.add_argument('from_date', type=str,
+		                    default=(datetime.datetime.today() - datetime.timedelta(days=1)).strftime('%d-%b-%Y'))
+		parser.add_argument('sales_order_type', type=str, default='sales_order_invoice')
 
 	def handle(self, *args, **options):
 		from_date = options.get('from_date')
-		sales_order_type = options.get('sales_order_type', 'sales_order_invoice')
-
-		if not from_date:
-			from_date = datetime.datetime.today() - datetime.timedelta(days=1)
-			from_date = from_date.strftime('%d-%b-%Y')
+		sales_order_type = options.get('sales_order_type')
 
 		variables = {
 			"variables":
