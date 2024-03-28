ed07432ffb6b70ec5ab9086d538de5cdeec14427
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2024-03-28 18:41:10 +0530

    Service Request Enums Update

--- service_request/enums.py
+++ service_request/enums.py
@@ -4,6 +4,8 @@ from django.db import models
 class ServiceRequestTypeEnum(models.TextChoices):
 	CHANGE_PHONE_NUMBER = 'CHANGE_PHONE_NUMBER', 'Change Phone Number'
 	UID_UPLOAD_FOR_EKYC = 'UID_UPLOAD_FOR_EKYC', 'UID Upload For EKyc'
+	UPDATE_ADDRESS = 'UPDATE_ADDRESS', 'Update Address'
+	CHANGE_CYLINDER = 'CHANGE_CYLINDER', 'Change Cylinder'
 
 
 class ServiceRequestTypeStatusEnum(models.TextChoices):
