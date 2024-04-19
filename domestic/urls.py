8f8951f3d4b02d6b27398f330ced5d7f4b052553
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2024-04-19 18:20:17 +0530

    Domestic App Templates Update

--- domestic/urls.py
+++ domestic/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from connection_app.views import CustomerProfileSearchView
 from domestic import views
 from domestic.views import DomesticApplicationWebFormView, DomesticApplicationIframeWebFormView
 
@@ -9,7 +10,7 @@ from domestic.views import DomesticApplicationWebFormView, DomesticApplicationIf
 router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('', views.DomesticApplicationStatusView.as_view(), name="index"),
+    path('', CustomerProfileSearchView.as_view(), name="index"),
     path('', include(router.urls)),
     path('portal/web-form/', DomesticApplicationWebFormView.as_view(), name="web_form"),
     path('portal/i-web-form/', DomesticApplicationIframeWebFormView.as_view(), name="i_web_form"),
