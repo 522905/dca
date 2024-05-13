1c866bda6a6d9abc422cc4c05ff55e91687a135f
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2024-05-13 11:48:23 +0530

    Connection App Sales Order Scheduler

--- connection_app/management/commands/connection_app_sales_order_scheduler.py
+++ connection_app/management/commands/connection_app_sales_order_scheduler.py
@@ -6,26 +6,28 @@ from ujjwala.camunda_functions import start_process_in_camunda_v2
 
 
 class Command(BaseCommand):
-	def add_arguments(self, parser):
-		# Positional arguments
-		parser.add_argument('-fd', '--from_date', type=str,
-		                    default=(datetime.datetime.today() - datetime.timedelta(days=1)).strftime('%d-%b-%Y'))
-		parser.add_argument('-td', '--to_date', type=str, default=datetime.datetime.today().strftime('%d-%b-%Y'))
-		parser.add_argument('-sot', '--sales_order_type', type=str, default='sales_order_invoice')
+        def add_arguments(self, parser):
+                # Positional arguments
+                parser.add_argument('-fd', '--from_date', type=str,
+                                    default=(datetime.datetime.today() - datetime.timedelta(days=1)).strftime(
+	                                    '%d-%b-%Y'))
+                parser.add_argument('-td', '--to_date', type=str,
+                                    default=datetime.datetime.today().strftime('%d-%b-%Y'))
+                parser.add_argument('-sot', '--sales_order_type', type=str, default='sales_order')
 
-	def handle(self, *args, **options):
-		from_date = options.get('from_date')
-		to_date = options.get('to_date')
-		sales_order_type = options.get('sales_order_type')
+        def handle(self, *args, **options):
+                from_date = options.get('from_date')
+                to_date = options.get('to_date')
+                sales_order_type = options.get('sales_order_type')
 
-		variables = {
-			"variables":
-				{
-					"from_date": {"value": from_date, "type": "String"},
-					"to_date": {"value": to_date, "type": "String"},
-					"sales_order_type": {"value": sales_order_type, "type": "String"},
-				}
-		}
+                variables = {
+                        "variables":
+                                {
+                                        "from_date": {"value": from_date, "type": "String"},
+                                        "to_date": {"value": to_date, "type": "String"},
+                                        "sdms_task": {"value": sales_order_type, "type": "String"},
+                                }
+                }
 
-		res = start_process_in_camunda_v2('Process_domestic_app', variables=variables)
-		print(res)
+                res = start_process_in_camunda_v2('Process_domestic_app', variables=variables)
+                print(res)
