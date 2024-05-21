ef8b9a2a32dd5607691417d2d31d105c8df38f9a
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2024-05-21 16:38:32 +0530

    Image Utils

--- app_utilities/templatetags/img_utils.py
+++ app_utilities/templatetags/img_utils.py
@@ -1,6 +1,6 @@
 from django import template
 
-from domestic_app.settings import THUMBOR_WEB_URL
+from domestic_app.settings import THUMBOR_WEB_URL, THUMBOR_LOCAL_URL
 
 register = template.Library()
 
@@ -10,4 +10,6 @@ def resize(url, fit_in_size):
 	"""return f"http://dca.arungas.com:6988/unsafe/fit-in/{fit_in_size}/filters:format(webp)/{url}"""
 	if not url:
 		return ''
-	return f"{THUMBOR_WEB_URL}/unsafe/fit-in/{fit_in_size}/filters:format(jpeg)/{url}"
+	w, h = fit_in_size.split("x")
+	# return f"{THUMBOR_LOCAL_URL}/unsafe/fit-in/{w}x0:{h}x0/filters:format(jpeg)/{url}"
+	return f"{THUMBOR_LOCAL_URL}/unsafe/{w}x0:{h}x0/filters:format(jpeg)/{url}"
