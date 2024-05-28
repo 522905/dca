8adafd8b3febb364a3215c0b3924bfc5147e8dfb
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2024-05-28 15:41:03 +0530

    Template Tags Connection App

--- connection_app/templatetags/connection_app_utils.py
+++ connection_app/templatetags/connection_app_utils.py
@@ -8,3 +8,13 @@ register = template.Library()
 @register.filter()
 def has_post_inspection_permission(user):
 	return can_do_post_inspection(user)
+
+
+@register.filter()
+def query_filter(value, attr):
+	return value.filter(**eval(attr))
+
+
+@register.filter
+def get(mapping, key):
+	return mapping.get(key, '')
