9fb9ed36e212d90b5cc1d20aab6f1771c0a27789
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2024-06-18 11:29:44 +0530

    Service Request List View

--- service_request/models.py
+++ service_request/models.py
@@ -17,6 +17,9 @@ class ServiceRequest(models.Model):
 	status = models.CharField(max_length=128, choices=ServiceRequestTypeStatusEnum.choices,
 	                          default=ServiceRequestTypeStatusEnum.PENDING)
 	remarks = models.TextField(null=True, blank=True)
+	reviewed_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True, related_name="reviewed_by")
+	reviewed_on = models.DateTimeField(null=True)
+	sdms_ticket_number = models.CharField(max_length=52, null=True, blank=True)
 
 	class Meta:
 		permissions = (
