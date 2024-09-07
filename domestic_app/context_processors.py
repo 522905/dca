ba5ccca5b4b1bf0d7f646d7e3fbe329fc9afb63c
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2024-09-07 16:09:54 +0530

    Context processors Update

--- domestic_app/context_processors.py
+++ domestic_app/context_processors.py
@@ -1,3 +1,6 @@
+from domestic_app import settings
+
+
 def bridge_context(request):
 	bridge_link = request.headers.get('Bridge-Location', '')
 
@@ -5,3 +8,7 @@ def bridge_context(request):
 		'bridge_available': True if bridge_link else False,
 		'bridge_link': bridge_link
 	}
+
+
+def thumbor_compression_url(request):
+	return {'THUMBOR_COMPRESSION_URL': settings.THUMBOR_URL}
