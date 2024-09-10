5528ec75832a3a68bb5413f133d1c52e5c105de8
Ashish <ashishchumber34@gmail.com>
2024-09-10 17:32:04 +0530

    changes to flow view to reply through response json

--- communication_log/jobs.py
+++ communication_log/jobs.py
@@ -1,4 +1,5 @@
 import io
+import json
 import re
 from functools import wraps
 
@@ -18,6 +19,7 @@ from domestic_app.utils import get_minio_public_url
 # from ujjwala.jobs import ensure_db_connection
 from django.db import close_old_connections
 from ujjwala.management.commands.ujjwala_file_worker import upload_compressed_file_to_tus
+from ujjwala.views import  WhatsappPreInspection
 import logging
 
 # Initialize logger
@@ -326,15 +328,23 @@ def detect_intent_texts(project_id, session_id, text, language_code='hi'):
 def chatbot_view(data_dict):
     data = data_dict['data']
     message_data = data.get('message', {})
+    message_content = message_data["message_content_type"]
     user_message = redis_image(message_data['media_url']) if 'media_url' in message_data else message_data.get('message', ' ')
     project_id = 'om-prakash-rerm'
     user_id = data['customer']["id"]
     session_id = data['customer']['phone_number']
     print(f'the user pin is : {user_message}')
-    logger.info(f"Sending message to Dialogflow: {user_message} from {session_id}")
-    # You can use a unique ID for each user session
-    response = detect_intent_texts(project_id, session_id, user_message)
-    reply = response.fulfillment_text
+    if message_content == "InteractiveFlowReply":
+        response_json_data = json.loads(user_message["nfm_reply"]["response_json"])
+        logger.info(f'the response data that we get is : {response_json_data.get("data")}')
+        reply = WhatsappPreInspection(Inspection_data=response_json_data.get("data" ,""), unique_id=session_id, intent="address_details")
+        logger.info(f'handling flow response to fill the address form')
+    else:
+        logger.info(f"Sending message to Dialogflow: {user_message} from {session_id}")
+        # You can use a unique ID for each user session
+        response = detect_intent_texts(project_id, session_id, user_message)
+        reply = response.fulfillment_text
+
     if reply:
         try:
             dialogflow_whatapp_message(reply, session_id, user_id)
