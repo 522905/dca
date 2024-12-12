862574147c53775221aac31b8bb7fcdf7176c6ff
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2024-12-12 12:34:14 +0530

    Connection App Mobile 2 Field

--- connection_app/models.py
+++ connection_app/models.py
@@ -58,6 +58,7 @@ class ConnectionApplication(models.Model):
 	updated_on = models.DateTimeField(auto_now=True)
 	name = models.CharField(max_length=255, null=True)
 	mobile = models.CharField(max_length=10)
+	mobile2 = models.CharField(max_length=10, null=True, blank=True)
 	address = models.TextField(null=True, blank=True)
 	address_json = models.JSONField(null=True, blank=True)
 	application_type = models.CharField(max_length=25, choices=ApplicationTypeEnum.choices)
