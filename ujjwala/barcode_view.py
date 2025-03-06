56dcd357849132c5b2e602c4da1a3b389f07af48
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2025-03-06 18:31:34 +0530

    Customer Data In Barcode View

--- ujjwala/barcode_view.py
+++ ujjwala/barcode_view.py
@@ -49,7 +49,14 @@ class NewBarCodeLabelPrintView(FormView):
 		except Exception as e:
 			return JsonResponse({"status": "error", "message": f"Error processing PRN file: {str(e)}"}, status=500)
 
-		return JsonResponse({"status": "success", "prn_data": file_data.decode(), "customer_profile": application})
+		# Serialize the application object
+		customer_data = {
+			"name": application.name,
+			"consumer_id": application.consumer_id,
+			"address": application.address,
+			"referral_code": application.referral_code,
+		}
+		return JsonResponse({"status": "success", "prn_data": file_data.decode(), "customer_profile": customer_data})
 
 	def create_context_data(self, obj: CustomerProfile) -> dict:
 		"""Generates the context dictionary with consumer and address details."""
