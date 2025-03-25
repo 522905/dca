bf70c4fa3640281d22e6d6232e1effdae022673a
Sandeep Uniyal <sandeep.uniyal@arungas.com>
2025-03-25 13:48:58 +0530

    Fixed Not Found In Cancelled Update Sales Order

--- connection_app/camunda_functions.py
+++ connection_app/camunda_functions.py
@@ -915,8 +915,9 @@ def update_returned_booked_order(sales_order_id, status):
 
 	so_obj.save()
 
-	if status in ['CANCELLED']:
-		start_process_fetch_sales_order_details_from_sdms(so_obj.id, so_obj.full_filled_by_distributor.code)
+	if status in ['NOT_FOUND', 'CANCELLED']:
+		if so_obj.full_filled_by_distributor:
+			start_process_fetch_sales_order_details_from_sdms(so_obj.id, so_obj.full_filled_by_distributor.code)
 
 	return variables
 
